0a0159d722b89ac44595534237ca12615281bd3a
Jianjin Zhang <583247597@qq.com>
2019-03-18 17:00:11 +0800

    基础布局

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+  <div class="container">
+    <p class="float-left">
+      由 <a href="https://weibo.com/u/5499994695" target="_blank">章健瑾</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+    </p>
+
+    <p class="float-right"><a href="mailto:583247597@qq.com">联系我们</a></p>
+  </div>
+</footer>
