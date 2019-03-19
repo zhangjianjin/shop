210b9d528d79e4d8aa18c6a680906aff82265bda
Jianjin Zhang <583247597@qq.com>
2019-03-19 18:26:49 +0800

    评价商品

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Events\OrderReviewed;
+use App\Listeners\UpdateProductRating;
 use App\Listeners\SendOrderPaidMail;
 use App\Events\OrderPaid;
 use App\Listeners\UpdateProductSoldCount;
@@ -27,6 +29,9 @@ class EventServiceProvider extends ServiceProvider
         OrderPaid::class => [
             UpdateProductSoldCount::class,
 	    SendOrderPaidMail::class,
+        ],
+        OrderReviewed::class => [
+            UpdateProductRating::class,
         ],
     ];
 
