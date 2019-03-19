9cc43b32c5c9217bffea98fd098b3bfda271cc21
Jianjin Zhang <583247597@qq.com>
2019-03-19 14:51:38 +0800

    购物车页面

--- routes/web.php
+++ routes/web.php
@@ -30,7 +30,10 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::post('products/{product}/favorite', 'ProductsController@favor')->name('products.favor');
     Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
     Route::get('products/favorites', 'ProductsController@favorites')->name('products.favorites');
+    //购物车
     Route::post('cart', 'CartController@add')->name('cart.add');
+    Route::get('cart', 'CartController@index')->name('cart.index');
+    Route::delete('cart/{sku}', 'CartController@remove')->name('cart.remove');
 
 });
 
