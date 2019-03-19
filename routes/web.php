c90ec3d4155eb9df4d3ca8c40aba22d0915fc6ea
Jianjin Zhang <583247597@qq.com>
2019-03-19 15:07:02 +0800

    创建订单

--- routes/web.php
+++ routes/web.php
@@ -34,6 +34,8 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::post('cart', 'CartController@add')->name('cart.add');
     Route::get('cart', 'CartController@index')->name('cart.index');
     Route::delete('cart/{sku}', 'CartController@remove')->name('cart.remove');
+    //订单
+    Route::post('orders', 'OrdersController@store')->name('orders.store');
 
 });
 
