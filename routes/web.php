afd861962340a4476b5c5c3f7e9909a87f9c1d45
Jianjin Zhang <583247597@qq.com>
2019-03-20 10:54:42 +0800

    用户界面 - 检查优惠券

--- routes/web.php
+++ routes/web.php
@@ -49,6 +49,8 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::post('orders/{order}/apply_refund', 'OrdersController@applyRefund')->name('orders.apply_refund');
     //退款
     Route::post('payment/wechat/refund_notify', 'PaymentController@wechatRefundNotify')->name('payment.wechat.refund_notify');
+    //优惠券
+    Route::get('coupon_codes/{code}', 'CouponCodesController@show')->name('coupon_codes.show');
 
 });
 Route::post('payment/alipay/notify', 'PaymentController@alipayNotify')->name('payment.alipay.notify');
