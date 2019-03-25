499e513501bd063845730d73daeae6380e74617a
Jianjin Zhang <583247597@qq.com>
2019-03-25 10:54:39 +0800

    初始化众筹模型

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,9 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
+    const TYPE_NORMAL = 'normal';
+    const TYPE_CROWDFUNDING = 'crowdfunding';
+    public static $typeMap = [
+        self::TYPE_NORMAL  => '普通商品',
+        self::TYPE_CROWDFUNDING => '众筹商品',
+    ];
+
     protected $fillable = [
                     'title', 'description', 'image', 'on_sale', 
-                    'rating', 'sold_count', 'review_count', 'price'
+                    'rating', 'sold_count', 'review_count', 'price', 'type',
     ];
     protected $casts = [
         'on_sale' => 'boolean', // on_sale 是一个布尔类型的字段
@@ -32,5 +39,10 @@ class Product extends Model
             return $this->attributes['image'];
         }
         return \Storage::disk('public')->url($this->attributes['image']);
+    }
+
+    public function crowdfunding()
+    {
+        return $this->hasOne(CrowdfundingProduct::class);
     }
 }
